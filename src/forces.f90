6c4b8641916e036b6ffa3be30a88156843e31d41
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2021-11-30 17:10:54 +0100

    fix debugging output from forces()

--- src/forces.f90
+++ src/forces.f90
@@ -63,26 +63,27 @@ SUBROUTINE forces
   if (dbg_forces) then
   
     call add_real_3d("lu_e",        ns, nzeta, ntheta3, lu_e     )
-    call add_real_3d("lv_e",        ns, nzeta, ntheta3, lv_e     )
-    call add_real_3d("guu",         ns, nzeta, ntheta3, guu      )
-    call add_real_3d("guv",         ns, nzeta, ntheta3, guv      )
-    call add_real_3d("gvv",         ns, nzeta, ntheta3, gvv      )
+    call add_real_3d("lv_e_in",     ns, nzeta, ntheta3, lv_e     )
+    call add_real_3d("guu_in",      ns, nzeta, ntheta3, guu      )
+    call add_real_3d("guv_in",      ns, nzeta, ntheta3, guv      )
+    call add_real_3d("gvv_in",      ns, nzeta, ntheta3, gvv      )
     call add_real_3d("ru12",        ns, nzeta, ntheta3, ru12     )
     call add_real_3d("zu12",        ns, nzeta, ntheta3, zu12     )
-    call add_real_3d("brmn_e",      ns, nzeta, ntheta3, brmn_e   )
-    call add_real_3d("bzmn_e",      ns, nzeta, ntheta3, bzmn_e   )
-    call add_real_3d("r1",          ns, nzeta, ntheta3, r1       )
-    call add_real_3d("z1",          ns, nzeta, ntheta3, z1       )
-    call add_real_3d("ru",          ns, nzeta, ntheta3, ru       )
-    call add_real_3d("zu",          ns, nzeta, ntheta3, zu       )
+    call add_real_3d("brmn_e_in",   ns, nzeta, ntheta3, brmn_e   )
+    call add_real_3d("bzmn_e_in",   ns, nzeta, ntheta3, bzmn_e   )
     call add_real_3d("ru0",         ns, nzeta, ntheta3, ru0      )
     call add_real_3d("zu0",         ns, nzeta, ntheta3, zu0      )
-    call add_real_3d("rcon_even_m", ns, nzeta, ntheta3, rcon(:,0))
-    call add_real_3d("zcon_even_m", ns, nzeta, ntheta3, zcon(:,0))
     call add_real_3d("rcon0",       ns, nzeta, ntheta3, rcon0    )
     call add_real_3d("zcon0",       ns, nzeta, ntheta3, zcon0    )
     call add_real_3d("gcon",        ns, nzeta, ntheta3, gcon     )
 
+    call add_real_4d("r1",   ns, 2, nzeta, ntheta3, r1,   order=(/ 1, 3, 4, 2 /) )
+    call add_real_4d("z1",   ns, 2, nzeta, ntheta3, z1,   order=(/ 1, 3, 4, 2 /) )
+    call add_real_4d("ru",   ns, 2, nzeta, ntheta3, ru,   order=(/ 1, 3, 4, 2 /) )
+    call add_real_4d("zu",   ns, 2, nzeta, ntheta3, zu,   order=(/ 1, 3, 4, 2 /) )
+    call add_real_4d("rcon_in", ns, 2, nzeta, ntheta3, rcon, order=(/ 1, 3, 4, 2 /) )
+    call add_real_4d("zcon_in", ns, 2, nzeta, ntheta3, zcon, order=(/ 1, 3, 4, 2 /) )
+    
     if (lthreed) then
       call add_real_4d("rv", ns, 2, nzeta, ntheta3, rv, order=(/ 1, 3, 4, 2 /) )
       call add_real_4d("zv", ns, 2, nzeta, ntheta3, zv, order=(/ 1, 3, 4, 2 /) )
@@ -150,6 +151,14 @@ SUBROUTINE forces
      lu_o(l)   = lu_o(l) + lu_o(l+1)
   END DO
 
+  if (dbg_forces) then
+    ! save data before it gets overwritten again
+    ! due to array re-usage
+    
+    call add_real_3d("lu_o",     ns, nzeta, ntheta3, lu_o  )
+    call add_real_3d("lv_e_out", ns, nzeta, ntheta3, lv_e  )
+  end if
+  
   guu(1:nrzt)  = guu(1:nrzt) * sqrts(1:nrzt)**2
   bsqr(1:nrzt) = gvv(1:nrzt) * sqrts(1:nrzt)**2
 
@@ -216,15 +225,6 @@ SUBROUTINE forces
     call add_real_3d("azmn_o", ns, nzeta, ntheta3, azmn_o)
     call add_real_3d("bzmn_e", ns, nzeta, ntheta3, bzmn_e)
     call add_real_3d("bzmn_o", ns, nzeta, ntheta3, bzmn_o)
-    call add_real_3d("guu",    ns, nzeta, ntheta3, guu   )
-    call add_real_3d("guus",   ns, nzeta, ntheta3, guus  )
-    call add_real_3d("guv",    ns, nzeta, ntheta3, guv   )
-    call add_real_3d("guvs",   ns, nzeta, ntheta3, guvs  )
-    call add_real_3d("gvv",    ns, nzeta, ntheta3, gvv   )
-    call add_real_3d("gvvs",   ns, nzeta, ntheta3, gvvs  )
-    call add_real_3d("bsqr",   ns, nzeta, ntheta3, bsqr  )
-    call add_real_3d("lu_o",   ns, nzeta, ntheta3, lu_o  )
-    call add_real_3d("lv_e",   ns, nzeta, ntheta3, lv_e  )
     if (lthreed) then
       call add_real_3d("crmn_e", ns, nzeta, ntheta3, crmn_e)
       call add_real_3d("crmn_o", ns, nzeta, ntheta3, crmn_o)
@@ -236,9 +236,17 @@ SUBROUTINE forces
       call add_null("czmn_e")
       call add_null("czmn_o")
     end if
-
-    call add_real_4d("rcon", ns, 2, nzeta, ntheta3, rcon, order=(/ 1, 3, 4, 2 /) )
-    call add_real_4d("zcon", ns, 2, nzeta, ntheta3, zcon, order=(/ 1, 3, 4, 2 /) )
+    
+    call add_real_3d("guu_out",  ns, nzeta, ntheta3, guu   )
+    call add_real_3d("guus",     ns, nzeta, ntheta3, guus  )
+    call add_real_3d("guv_out",  ns, nzeta, ntheta3, guv   )
+    call add_real_3d("guvs",     ns, nzeta, ntheta3, guvs  )
+    call add_real_3d("gvv_out",  ns, nzeta, ntheta3, gvv   )
+    call add_real_3d("gvvs",     ns, nzeta, ntheta3, gvvs  )
+    call add_real_3d("bsqr",     ns, nzeta, ntheta3, bsqr  )
+   
+    call add_real_4d("rcon_out", ns, 2, nzeta, ntheta3, rcon, order=(/ 1, 3, 4, 2 /) )
+    call add_real_4d("zcon_out", ns, 2, nzeta, ntheta3, zcon, order=(/ 1, 3, 4, 2 /) )
 
     call close_dbg_out()
   end if

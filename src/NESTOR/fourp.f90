9ee1a4a6b04b00aeee8a9a70aafac2e6b6fed00a
Jonathan Schilling <jons@proximafusion.com>
2023-10-28 10:09:47 +0200

    comments and cleanup in fourp

--- src/NESTOR/fourp.f90
+++ src/NESTOR/fourp.f90
@@ -7,9 +7,9 @@
 !> @param grp
 SUBROUTINE fourp (grpmn, grp)
   USE vacmod, vm_grpmn => grpmn
-  
+
   use dbgout
-  
+
   IMPLICIT NONE
 
   REAL(rprec), INTENT(inout) :: grpmn(0:mf,-nf:nf,nuv2,ndim)
@@ -32,11 +32,13 @@ SUBROUTINE fourp (grpmn, grp)
      !
      ! THE 3rd INDEX OF GRPMN IS THE PRIMED U,V MESH COORDINATE
      DO kv = 1,nv
+        iuv = kv+nv*(ku-1)
+        ireflect = imirr(iuv)
+
         DO n = 0,nf
            cosn = p5*onp*cosv(n,kv)
            sinn = p5*onp*sinv(n,kv)
-           iuv = kv+nv*(ku-1)
-           ireflect = imirr(iuv)
+
            DO isym = 1, ndim ! ndim == 1 for stellarator-symmetry, 2 for asymmetric (lasym=T)
               DO ip = 1,nuv2
                  IF (isym .eq. 1) THEN ! only contrib for stellarator-symmetry
@@ -53,17 +55,17 @@ SUBROUTINE fourp (grpmn, grp)
 !                         ! fourp_kernel.dat
 !                         print *, iuv, ip, kernel
 !                      end if
-                 ELSE IF (isym .eq. 2) THEN
+                 ELSEIF (isym .eq. 2) THEN
                     ! symmetric part
                     kernel = grp(iuv,ip) + grp(ireflect,ip)
                  END IF
 
                  g1(ip,n,isym)=g1(ip,n,isym) + cosn*kernel
                  g2(ip,n,isym)=g2(ip,n,isym) + sinn*kernel
-              END DO
-           END DO
-        END DO
-     END DO
+              END DO ! ip
+           END DO ! isym
+        END DO ! n
+     END DO ! kv
 
      ! PERFORM KU (POLOIDAL ANGLE) TRANFORM [COMPLETE SIN(mu-nv) / COS(mu-nv) TRANSFORM]
      DO m = 0,mf
@@ -71,28 +73,29 @@ SUBROUTINE fourp (grpmn, grp)
            IF (isym .EQ. 1) THEN
               cosm = -cosui(m,ku)
               sinm =  sinui(m,ku)
-           ELSE IF (isym .EQ. 2) THEN
+           ELSEIF (isym .EQ. 2) THEN
               sinm = cosui(m,ku)
               cosm = sinui(m,ku)
            END IF
-           
+
            DO n= 0,nf
              DO ip = 1,nuv2
                  gcos = g1(ip,n,isym)*sinm
-                 gsin = g2(ip,n,isym)*cosm
+                 gsin = g2(ip,n,isym)*cosm ! has -1
                  grpmn(m, n,ip,isym)    = grpmn(m, n,ip,isym) + gcos + gsin
                  IF (n .NE. 0) THEN
                     grpmn(m,-n,ip,isym) = grpmn(m,-n,ip,isym) + gcos - gsin
                  ENDIF
-             END DO
-           END DO
-        END DO
-     END DO
+             END DO ! ip
+           END DO ! n
+        END DO ! isym
+     END DO ! m
 
-  END DO
-  
+  END DO ! ku
+
   if (open_dbg_context("vac1n_fourp", id=icall)) then
-    
+
+    ! NOTE: This is the sum of the results from analyt and fourp!
     call add_real_4d("grpmn", mf1, nf1, nv, nu3, grpmn)
 
     call close_dbg_out()
